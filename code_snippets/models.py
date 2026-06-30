8c398d4a3308e9c07172334080d944aae5afe24f
StefanKraev <stefanemilovkraev@abv.bg>
2026-06-30 22:46:06 +0300

    Added models for the codesnippet and its category.

--- code_snippets/models.py
+++ code_snippets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class CodeSnippet(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField() 
+    developer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippets')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='snippets')
+
+    def __str__(self):
+        return self.title
