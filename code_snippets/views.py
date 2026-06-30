57b86ecd4b9adfe0994ecbad135f74cfeb0efecc
StefanKraev <stefanemilovkraev@abv.bg>
2026-06-30 21:38:04 +0300

    Added url and template for registering users. Added link in the navbar to the register page

--- code_snippets/views.py
+++ code_snippets/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save() # Създава потребителя в базата данни
+            messages.success(request, 'Account created successfully!')
+            return redirect('home') # Пренасочва след регистрация
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
